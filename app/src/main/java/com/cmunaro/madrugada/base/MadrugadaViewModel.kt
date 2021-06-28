133c98bd1f496ecd1c771f8f84da1a6732896a19
Munez07 <munaro07@gmail.com>
2021-06-29 00:12:26 +0200

    Hide implementation from MadrugadaViewModel.kt

--- app/src/main/java/com/cmunaro/madrugada/base/MadrugadaViewModel.kt
+++ app/src/main/java/com/cmunaro/madrugada/base/MadrugadaViewModel.kt
@@ -5,21 +5,13 @@ import androidx.lifecycle.viewModelScope
 import com.cmunaro.madrugada.base.pattern_matching.MadrugadaStateFlow
 import com.cmunaro.madrugada.base.pattern_matching.MadrugadaStateFlowDSL
 import com.cmunaro.madrugada.base.pattern_matching.MadrugadaStateFlowDSLImpl
-import com.cmunaro.madrugada.base.pattern_matching.Matcher
-import kotlinx.coroutines.launch
 
 open class MadrugadaViewModel<S : MadrugadaState>(initialState: S) : ViewModel() {
+    open val state: MadrugadaStateFlow<S> = MadrugadaStateFlow.init(initialState)
+
     operator fun invoke(
         initializer: MadrugadaStateFlowDSL<S>.() -> Unit
     ) {
-        MadrugadaStateFlowDSLImpl<S>()
-            .apply(initializer)
-            .matchersPartialState.forEach { matcher: Matcher<S> ->
-                viewModelScope.launch {
-                    state.collectOnChangesOf(matcher)
-                }
-            }
+        MadrugadaStateFlowDSLImpl.get(viewModelScope, initializer, state)
     }
-
-    open val state: MadrugadaStateFlow<S> = MadrugadaStateFlow.init(initialState)
 }
--- app/src/main/java/com/cmunaro/madrugada/base/pattern_matching/MadrugadaStateFlowDSL.kt
+++ app/src/main/java/com/cmunaro/madrugada/base/pattern_matching/MadrugadaStateFlowDSL.kt
@@ -5,6 +5,8 @@ import com.cmunaro.madrugada.base.pattern_matching.full.PatternMatchFullStateCha
 import com.cmunaro.madrugada.base.pattern_matching.full.PatternMatchFullStateChangeInterface
 import com.cmunaro.madrugada.base.pattern_matching.partial.PatternMatchPartialStateChange
 import com.cmunaro.madrugada.base.pattern_matching.partial.PatternMatchPartialStateChangeInterface
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kotlin.reflect.KProperty1
 
 @DslMarker
@@ -26,4 +28,20 @@ abstract class MadrugadaStateFlowDSL<S : MadrugadaState>(
     ),
     PatternMatchFullStateChangeInterface<S> by PatternMatchFullStateChange(matchersPartialState)
 
-class MadrugadaStateFlowDSLImpl<S : MadrugadaState> : MadrugadaStateFlowDSL<S>(ArrayList())
+class MadrugadaStateFlowDSLImpl<S : MadrugadaState>private constructor() : MadrugadaStateFlowDSL<S>(ArrayList()) {
+    companion object {
+        fun<S: MadrugadaState> get(
+            viewModelScope: CoroutineScope,
+            initializer: MadrugadaStateFlowDSL<S>.() -> Unit,
+            state: MadrugadaStateFlow<S>
+        ) {
+            MadrugadaStateFlowDSLImpl<S>()
+                .apply(initializer)
+                .matchersPartialState.forEach { matcher: Matcher<S> ->
+                    viewModelScope.launch {
+                        state.collectOnChangesOf(matcher)
+                    }
+                }
+        }
+    }
+}
